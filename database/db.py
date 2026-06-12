297429f93c99eed8ccbbde2d6d1c4b6d301cf034
juanillo <jujemkv@proton.me>
2026-06-12 06:07:56 +0200

    creada la funcion para crear la tabla de la base de datos

---
+++ database/db.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+def connect():
+    return sqlite3.connect("collection")
+
+
+def create_table():
+    connection = connect()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS  media (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            type TEXT,
+            genre TEXT,
+            year INTEGER,
+            image TEXT,
+            rating INTEGER,
+        )
+    """)
+
+    connection.commit()
+    connection.close()
