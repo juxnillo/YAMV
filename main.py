cafd5dc05908bf6dc6a7a4f65ed0cd4b6fbd5872
juanillo <jujemkv@proton.me>
2026-06-12 17:38:09 +0200

    test de funcion de tabla SQL

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from database.db import create_table
+from database.db import create_table, get_media, add_media
 
 if __name__ == "__main__":
     create_table()
+    add_media("One Piece", "Anime", "Shonen", 1999, "images/op.jpg", 9)
+    print(get_media())
